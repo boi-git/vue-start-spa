cf9734c1aaa3576fecd029f8e34d9bd836054de5
boi-git <adhwa.aizat23@gmail.com>
2023-11-20 13:11:12 +0800

    new

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<nav-bar
+        :pages="pages"
+        :active-page="activePage"
+        :nav-link-click="(index) => (activePage = index)"
+      ></nav-bar>
+  
+      <page-viewer :page="pages[activePage]"></page-viewer>
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  </template>
+  
+  <script>
+  import PageViewer from "./components/PageViewer.vue";
+  import NavBar from "./components/NavBar.vue";
+  
+  export default {
+    components: {
+      PageViewer,
+      NavBar,
+    },
+    data() {
+      return {
+        theme: "dark",
+        activePage: 0,
+        pages: [
+          {
+            link: { text: "Rosters", url: "index.html" },
+            pageTitle: "Rosters Page",
+            content: "This the Rosters content",
+          },
+          {
+            link: { text: "Courts", url: "index.html" },
+            pageTitle: "Courts Page",
+            content: "This the Courts content",
+          },
+          {
+            link: { text: "Events", url: "index.html" },
+            pageTitle: "Events Page",
+            content: "This the Events content",
+          },
+          {
+            link: { text: "Shops", url: "index.html" },
+            pageTitle: "Shops Page",
+            content: "This the Shops content",
+          },
+        ],
+        courts: [
+          {
+            courtName: "Court A",
+            upcomingMatchName: "UBL Eagles vs UBL Eagles",
+            upcomingMatchDetails: "Thu-Nov. 11- 8:00pm",
+          },
+          {
+            courtName: "Court B",
+            upcomingMatchName: "UBL Eagles vs UBL Eagles",
+            upcomingMatchDetails: "Thu-Nov. 11- 8:00pm",
+          },
+          {
+            courtName: "Court C",
+            upcomingMatchName: "UBL Eagles vs UBL Eagles",
+            upcomingMatchDetails: "Thu-Nov. 11- 8:00pm",
+          },
+        ],
+      };
+    },
+  };
+  </script>
+  
---
+++ src/components/NavBar.vue
@@ -0,0 +1,52 @@
+<template>
+ <nav :class="['navbar-' + theme, 'bg-' + theme, 'navbar', 'navbar-expand-lg', 'border-bottom', 'border-2', 'border-danger', 'shadow']">
+                        <div class="container-fluid">
+                            <a href="" class="navbar-brand">
+                                <img class="img-fluid" src="../assets/logo.png" alt="" style="width: 50%;">
+                            </a>
+                            <ul class="navbar-nav mb-2 mb-lg-0 ml-2 ">
+                                <li v-for="(page,index) in pages" class="nav-item mx-2 " :key="index">
+                                    <a v-bind:href="page.link.url" 
+                                    v-bind:title="'This link goes to the ' + page.link.text + ' page'"
+                                    aria-current="page"
+                                    class="nav-link"
+                                    :class="{active: activePage==index}"
+                                    @click.prevent="navLinkClick(index)"
+                                    >{{ page.link.text}} 
+                                </a>
+                                </li>
+                                <form  class="d-flex">
+                                    <button class="btn btn-primary"
+                                    @click.prevent=changeTheme()>
+                                    Toggle</button>
+                                </form>
+                            </ul>                          
+                        </div>
+                    </nav>
+</template>
+
+<script>
+export default{
+    props:['pages','activePage','navLinkClick'],
+            data(){
+                return{
+                    theme:'light',
+                  
+
+                }
+                
+            },
+            methods: {
+                        changeTheme(){
+                            let theme = 'light'
+
+                            if(this.theme == 'light'){
+                                theme='dark';
+                            }
+
+                            this.theme=theme
+                        }
+            },
+    
+}
+</script>
